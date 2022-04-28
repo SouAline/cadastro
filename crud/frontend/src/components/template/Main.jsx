d8b8060c900488aa850c650469d6ab0af197fe16
Aline Souza <ds.alinesouza@gmail.com>
2022-04-27 22:57:06 -0300

    Update main component, create new file and home component

--- crud/frontend/src/components/template/Main.jsx
+++ crud/frontend/src/components/template/Main.jsx
@@ -5,6 +5,9 @@ import Header from "./Header";
 export default (props) => (
   <React.Fragment>
     <Header {...props}></Header>
-    <main className="content">Conteúdo</main>
+    <main className="content container-fluid">
+      {/* Para jogar as tags filhas é necessário usar props.children */}
+      <div className="p-3 mt-3">{props.children}</div>
+    </main>
   </React.Fragment>
 );
--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -12,7 +12,14 @@ export default (props) => (
   <div className="app">
     <Logo />
     <Nav />
-    <Main icon="home" title="Inicio" subtitle="Projeto para aprender React" />
+    <Main icon="home" title="Inicio" subtitle="Projeto para aprender React">
+      <div className="display-4">Bem vindo</div>
+      <hr />
+      <p className="mb-0">
+        Sistema para exemplificar a construção de um cadastro desenvolvido em
+        React
+      </p>
+    </Main>
     <Footer />
   </div>
 );
