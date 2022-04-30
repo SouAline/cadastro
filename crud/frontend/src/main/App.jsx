5b643bee26b577951f59583f1f030342d783fc06
Aline Souza <ds.alinesouza@gmail.com>
2022-04-29 21:16:29 -0300

    update project

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -5,21 +5,14 @@ import React from "react";
 
 import Logo from "../components/template/Logo";
 import Nav from "../components/template/Nav";
-import Main from "../components/template/Main";
+import Home from "../components/home/Home";
 import Footer from "../components/template/Footer";
 
 export default (props) => (
   <div className="app">
     <Logo />
     <Nav />
-    <Main icon="home" title="Inicio" subtitle="Projeto para aprender React">
-      <div className="display-4">Bem vindo</div>
-      <hr />
-      <p className="mb-0">
-        Sistema para exemplificar a construção de um cadastro desenvolvido em
-        React
-      </p>
-    </Main>
+    <Home />
     <Footer />
   </div>
 );
