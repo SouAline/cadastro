160bb1cbd6cfbade083bd206928dfe7be75cd961
Aline Souza <ds.alinesouza@gmail.com>
2022-04-29 21:47:27 -0300

    create routes components

---
+++ crud/frontend/src/main/Routes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../components/home/Home";
+import UserCrud from "../components/user/UserCrud";
+
+export default (props) => (
+  <Routes>
+    <Route exact path="/" component={Home}></Route>
+    <Route path="/users" component={UserCrud}></Route>
+    {/* implementar redirect aqui */}
+  </Routes>
+);
